b0eac18f56670b9dc75d433b7ed5d37ddca23163
François Poirotte <clicky@erebot.net>
2011-01-17 22:33:12 +0000

    Fix unit tests.

--- tests/AutoIdentTest.php
+++ tests/AutoIdentTest.php
@@ -29,7 +29,7 @@ extends ErebotModuleTestCase
     {
         // Make it look as though the channel
         // uses "password" as its key.
-        $this->_networkConfig
+        $this->_serverConfig
             ->expects($this->any())
             ->method('parseString')
             ->will($this->onConsecutiveCalls(
